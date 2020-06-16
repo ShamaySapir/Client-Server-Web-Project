f85d26489ca3132cda33b2122236424ff858f0e1
HReyes <reyes@post.bgu.ac.il>
2020-06-16 16:03:07 +0300

    fix get random recipes

--- src/handlers/recipesGetRandom.js
+++ src/handlers/recipesGetRandom.js
@@ -6,7 +6,7 @@ const {
 const getRandomRecipesHandler = async (req, res, next) => {
   try {
     // for random recipes https://api.spoonacular.com/recipes/random
-    const randomRecipesAPI = await getRandomRecipeData(req.body.number); // get the recipe info by given id
+    const randomRecipesAPI = await getRandomRecipeData(3); // get the recipe info by given id
     const previews = [];
     if (randomRecipesAPI.data.length != 0) {
       randomRecipesAPI.data.recipes.forEach((recipeData) => {
