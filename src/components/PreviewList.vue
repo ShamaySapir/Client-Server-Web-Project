a521760b1a508a2fbd487de777e824f0263ced7b
Sapir-Shamay <shamay.sapir@gmail.com>
2020-07-02 13:09:01 +0300

    start preview

--- src/components/PreviewList.vue
+++ src/components/PreviewList.vue
@@ -1,13 +1,31 @@
 <template>
+  <div>
     <div align="center">
       <h1>This is a preview list</h1>
     </div>
+    <div class="preview">
+      <Preview v-for="number in numbers" :key="number"></Preview>
+    </div>
+  </div>
 </template>
 <script>
+import Preview from "@/components/Preview.vue";
 export default {
-  name: "PreviewList"
+  name: "PreviewList",
+  data: function () {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6],
+    };
+  },
+  components: {
+    Preview,
+  },
 };
 </script>
 
 <style scoped>
+.preview {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
