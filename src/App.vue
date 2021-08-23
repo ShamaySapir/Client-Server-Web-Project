47d27a658094daab3c0f0497224ac3e88b2dd38f
ShamaySapir <shamay.sapir@gmail.com>
2021-08-23 12:51:41 +0300

    rename id

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
-  <v-app id="ReciPyjamot">
+  <v-app id="Recipies">
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="primary">
-      <v-toolbar-title style="width: 150px;" class="ml-0 pl-4">
+      <v-toolbar-title style="width: 150px" class="ml-0 pl-4">
         <span class="hidden-sm-and-down">ReciPyjamot</span>
       </v-toolbar-title>
-      <v-btn color="black" class="white--text" style="margin: 10px;" to="/home">
+      <v-btn color="black" class="white--text" style="margin: 10px" to="/home">
         <v-icon left>home</v-icon>Home
       </v-btn>
-      <v-btn
-        color="black"
-        class="white--text"
-        style="margin: 10px;"
-        to="/about"
-      >
+      <v-btn color="black" class="white--text" style="margin: 10px" to="/about">
         <v-icon left>info</v-icon>About
       </v-btn>
       <v-btn
         color="black"
         class="white--text"
-        style="margin: 10px;"
+        style="margin: 10px"
         to="/search"
       >
         <v-icon left>search</v-icon>Search
@@ -29,7 +24,7 @@
             <v-btn
               color="black"
               class="white--text"
-              style="margin: 10px;"
+              style="margin: 10px"
               v-bind="attrs"
               v-on="on"
             >
@@ -43,7 +38,7 @@
                 color="black"
                 id="loginButton"
                 class="white--text"
-                style="margin: 10px;"
+                style="margin: 10px"
                 to="/favorites"
               >
                 <v-icon left>favorite</v-icon>Favorites
@@ -54,7 +49,7 @@
                 color="black"
                 id="loginButton"
                 class="white--text"
-                style="margin: 10px;"
+                style="margin: 10px"
                 to="/myRecipes"
               >
                 <v-icon left>restaurant</v-icon>My Recipes
@@ -65,7 +60,7 @@
                 color="black"
                 id="loginButton"
                 class="white--text"
-                style="margin: 10px;"
+                style="margin: 10px"
                 to="/family"
               >
                 <v-icon left>group</v-icon>Family Recipes
@@ -80,7 +75,7 @@
         color="black"
         id="loginButton"
         class="white--text"
-        style="margin: 10px;"
+        style="margin: 10px"
         to="/login"
         v-if="!isLoggedIn"
       >
@@ -92,7 +87,7 @@
         color="black"
         id="loginButton"
         class="white--text"
-        style="margin: 10px;"
+        style="margin: 10px"
         v-if="isLoggedIn"
         to="/home"
       >
@@ -103,7 +98,7 @@
         v-if="!isLoggedIn"
         id="registerButton"
         class="white--text"
-        style="margin: 10px;"
+        style="margin: 10px"
         to="/register"
       >
         <v-icon left>assignment</v-icon>Register
